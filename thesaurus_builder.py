ec5a7aa0379810397527403887ca892d12bbde6e
Seva Peretiatko <peretyatkosewa06@mail.ru>
2025-06-19 11:56:04 +0300

    Made uploading data

--- thesaurus_builder.py
+++ thesaurus_builder.py
@@ -3,4 +3,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from transliterate import translit
 
+def build_author_thesaurus(org_id: str, similariti_coefficient: float = 0.8, surname_diff: int = 3):
+    INPUT_FILE = f"org_data/processed/{org_id}/publications.csv"
+    OUTPUT_FILE = f"org_data/processed/{org_id}/thesaurus_authors.txt"
 
+    print("Uploading data...")
+    df = pd.read_csv(INPUT_FILE)
