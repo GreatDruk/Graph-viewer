f2d755dfaf0f6c83e500071ad9a7c0624e5640c0
Seva Peretiatko <peretyatkosewa06@mail.ru>
2025-07-21 23:19:38 +0300

    Define graph area function

--- src/layout/graph_area.py
+++ src/layout/graph_area.py
@@ -2,3 +2,12 @@
 Module: graph_area
 Defines the main graph display area of application.
 """
+from dash import dcc, html
+import dash_cytoscape as cyto
+
+def graph_area(
+        elements,
+        basic_stylesheet,
+        metrics_bounds
+    ):
+    pass
