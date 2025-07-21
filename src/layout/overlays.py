0351dfd88cba0c0c74f811221029f0166f8718ea
Seva Peretiatko <peretyatkosewa06@mail.ru>
2025-07-21 23:38:47 +0300

    Define overlays function

--- src/layout/overlays.py
+++ src/layout/overlays.py
@@ -2,3 +2,11 @@
 Module: overlays
 Defines all overlay dialogs and stores for application-level state.
 """
+from dash import dcc, html
+
+def overlays(
+        elements,
+        org_map,
+        default_org
+    ):
+    pass
