d1c4b690f2957337c6aaee5865695aed849c8bee
Seva Peretiatko <peretyatkosewa06@mail.ru>
2025-07-21 23:53:23 +0300

    Define base function

--- src/layout/base.py
+++ src/layout/base.py
@@ -2,3 +2,11 @@
 Module: base
 Defines the main Dash application layout by composing sidebar, graph area, and overlay components.
 """
+from dash import html
+from .sidebar import sidebar
+from .graph_area import graph_area
+from .overlays import overlays
+from src.data_prepare import prepare_network_elements
+
+def base_layout(org_map, default_org):
+    pass
