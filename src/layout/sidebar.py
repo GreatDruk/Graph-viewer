7918aaee83c9ce8b7d52d076b9af1a8ff4e476b3
Seva Peretiatko <peretyatkosewa06@mail.ru>
2025-07-21 22:57:08 +0300

    Define sidebar function

--- src/layout/sidebar.py
+++ src/layout/sidebar.py
@@ -2,3 +2,18 @@
 Module: sidebar
 Defines the sidebar layout of application.
 """
+from dash import dcc, html
+import plotly.express as px
+
+def sidebar(
+        size_options,
+        color_options,
+        nodes,
+        edges,
+        num_publication,
+        total_cites,
+        h_index,
+        years,
+        counts_publication_by_year
+    ):
+    pass
