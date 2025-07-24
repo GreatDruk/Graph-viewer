4f04435a966aebadebab5b17e35fd7c27fafe9bf
Seva Peretiatko <peretyatkosewa06@mail.ru>
2025-07-24 23:18:26 +0300

    Initialize upload_org

---
+++ src/callbacks/upload_org.py
@@ -0,0 +1,5 @@
+"""""
+Module: upload_org
+Defines the server-side callback for loading and updating all main graph elements,
+sidebar metrics and controls when the selected organization changes.
+"""
